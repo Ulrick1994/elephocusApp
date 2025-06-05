d9b0b32b9ddcfb4da447ee82a8fb0dabbf7a0422
Ulrick1994 <152562566+Ulrick1994@users.noreply.github.com>
2025-06-05 09:34:26 -0600

    Add files via upload

--- components/BottomNavBar.js
+++ components/BottomNavBar.js
@@ -1,30 +1,26 @@
 import React from "react";
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { View, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 import { MaterialIcons, FontAwesome } from "@expo/vector-icons";
 
-const BottomNavBar = ({
-  onHomePress,
-  onBookPress,
-  onAddPress,
-  onListPress,
-  onSettingsPress,
-  navigation,
-}) => {
+const BottomNavBar = ({ navigation }) => {
   return (
     <View style={styles.bottomNavBar}>
       <TouchableOpacity onPress={() => navigation.navigate("Main")}>
         <FontAwesome name="home" size={24} color="white" />
       </TouchableOpacity>
+
       <TouchableOpacity onPress={() => navigation.navigate("Eventos")}>
         <FontAwesome name="calendar" size={24} color="white" />
       </TouchableOpacity>
+
       <TouchableOpacity onPress={() => navigation.navigate("CrearFlashcard")} style={styles.addButton}>
         <MaterialIcons name="add" size={32} color="white" />
       </TouchableOpacity>
-      {/* Redirige ahora a Eventos en lugar de CatalogoTemario */}
+
       <TouchableOpacity onPress={() => navigation.navigate("CatalogoTemario")}>
         <FontAwesome name="book" size={24} color="white" />
       </TouchableOpacity>
+
       <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
         <MaterialIcons name="settings" size={24} color="white" />
       </TouchableOpacity>
@@ -34,18 +30,21 @@ const BottomNavBar = ({
 
 const styles = StyleSheet.create({
   bottomNavBar: {
+    width: Dimensions.get("window").width,
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
     backgroundColor: "#222",
-    paddingVertical: 10,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
+    paddingVertical: 12,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
   addButton: {
     backgroundColor: "#9f8b9f",
     padding: 10,
-    borderRadius: 50,
+    borderRadius: 30,
   },
 });
 
